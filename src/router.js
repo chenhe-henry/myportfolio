2730641fb8345f8917bb069df05ec93aff781fcc
chenhe-henry <chenhe.elec@gmail.com>
2020-07-27 17:13:05 +1000

    history mode

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import About from "@/views/About.vue";
 Vue.use(Router);
 
 export default new Router({
+  mode: history,
   routes: [
     { path: "/", name: "homepage", component: HomePage },
     { path: "/about", name: "about", component: About },
