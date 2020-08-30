118253a7683cbb38bf15ec46ec1277a2322dc1a3
chenhe-henry <chenhe.elec@gmail.com>
2020-08-31 02:31:10 +1000

    update projects responsive

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -2,16 +2,28 @@
   <div>
     <Nav />
     <div class="projects">
-      <div v-for="project in projects" :key="project.id" class="project">
-        <div class="project__shape">
-          <img :src="getLogosUrl(project.img)" :alt="project.name" class="project__img" />
+      <div v-for="project in projects" :key="project.id" class="projects__project">
+        <div class="projects__project__shape">
+          <img
+            :src="getLogosUrl(project.img)"
+            :alt="project.name"
+            class="projects__project__shape-img"
+          />
         </div>
-        <div class="project__tech">
-          <p class="project__title">{{project.name}}</p>
-          <p>{{ project.techStack }}</p>
-          <p>{{ project.description }}</p>
-          <a :href="project.demoLink" target="_blank" class="project__link">Live Demo</a> |
-          <a :href="project.codeLink" target="_blank" class="project__link">Code Link</a>
+        <div class="projects__project__brief">
+          <p class="projects__project__brief-title">{{project.name}}</p>
+          <p class="projects__project__brief-tech">{{ project.techStack }}</p>
+          <p class="projects__project__brief-description">{{ project.description }}</p>
+          <a
+            :href="project.demoLink"
+            target="_blank"
+            class="projects__project__brief-link"
+          >Live Demo</a> |
+          <a
+            :href="project.codeLink"
+            target="_blank"
+            class="projects__project__brief-link"
+          >Code Link</a>
         </div>
       </div>
     </div>
@@ -45,7 +57,7 @@ export default {
           description: ` This is the first full-stack project I have done. A health
               tracking system which allows you to login and helps you to record
               your health data.`,
-          techStack: "HTML,CSS,Vue.js,gh-pages, local storage",
+          techStack: "HTML, CSS, Vue.js, gh-pages, local storage",
           demoLink: "https://chenhe-henry.github.io/health-tracking",
           codeLink: "https://github.com/chenhe-henry/health-tracking",
         },
@@ -57,7 +69,7 @@ export default {
               MongoDB and Mongoose. A web app allows you to login with Google
               account, search and get details of properties for rental.`,
           techStack:
-            "React.js,Express,Node.js,MongoDB,Google OAuth,Domain API,Google Map API",
+            "React.js, Express, Node.js, MongoDB, Google OAuth, Domain API, Google Map API",
           demoLink: "https://findhome101.herokuapp.com/",
           codeLink: "https://github.com/chenhe-henry/gaseifx01-project3",
         },
@@ -85,60 +97,99 @@ export default {
 
 <style lang="scss" scoped>
 .projects {
-  margin: 0 15vw;
+  margin: 0;
   display: grid;
-}
-.project {
-  display: flex;
-  border: 1px solid grey;
-  border-radius: 10px;
-  color: black;
-  text-decoration: none;
-  margin: 30px auto;
-  padding: 20px;
-  width: 100%;
-  transition: all 0.2s;
-  box-shadow: 0 16px 38px -12px rgba(0, 0, 0, 0.56),
-    0 4px 25px 0 rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2);
-  &:active {
-    transform: translateY(0);
+  @media (min-width: 768px) {
+    margin: 0 5vw;
   }
-  &__title {
-    background-image: linear-gradient(to right, #fff700, #00c3ff);
-    -webkit-background-clip: text;
-    color: transparent;
-    font-size: 3rem;
-    font-weight: 700;
+  @media (min-width: 1024px) {
+    margin: 0 10vw;
   }
-  &__img {
-    margin: -4vh 4vw 0 4vw;
-    height: 20vh;
-    width: 15vw;
-    border-radius: 5px;
-    box-shadow: 0 16px 38px -12px rgba(0, 0, 0, 0.56),
-      0 4px 25px 0 rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2);
-    transition: all 0.4s;
-    backface-visibility: hidden;
-    &:hover {
-      transform: scale(0.9);
-      filter: blur(3px) brightness(70%);
-    }
+  @media (min-width: 1440px) {
+    margin: 0 15vw;
   }
-  &__tech {
-    font-weight: 500;
-    margin: 20px;
-    text-align: left;
-    width: 40vw;
-  }
-  &__link {
+  &__project {
+    border: 1px solid grey;
+    border-radius: 10px;
     color: black;
     text-decoration: none;
-    font-weight: bold;
-  }
-
-  &__shape {
-    transform: translateX(-3rem);
-    position: relative;
+    margin: 30px auto;
+    width: 100%;
+    transition: all 0.2s;
+    box-shadow: 0 16px 38px -12px rgba(0, 0, 0, 0.56),
+      0 4px 25px 0 rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2);
+    @media (min-width: 768px) {
+      width: 90%;
+    }
+    @media (min-width: 1024px) {
+      display: flex;
+      padding: 20px;
+    }
+    &:active {
+      transform: translateY(0);
+    }
+    &__shape {
+      position: relative;
+      @media (min-width: 1024px) {
+        transform: translateX(-3rem);
+      }
+      &-img {
+        margin: 2vh;
+        height: 35vh;
+        width: 90%;
+        border-radius: 5px;
+        box-shadow: 0 16px 38px -12px rgba(0, 0, 0, 0.56),
+          0 4px 25px 0 rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2);
+        transition: all 0.4s;
+        backface-visibility: hidden;
+        @media (min-width: 768px) {
+          height: 45vh;
+        }
+        @media (min-width: 1024px) {
+          height: 25vh;
+          width: 25vw;
+          margin: -4vh 4vw 0 4vw;
+        }
+        @media (min-width: 1024px) {
+          width: 20vw;
+        }
+        &:hover {
+          transform: scale(0.9);
+          filter: blur(3px) brightness(70%);
+        }
+      }
+    }
+    &__brief {
+      font-weight: 500;
+      margin: 20px;
+      text-align: center;
+      width: 80vw;
+      @media (min-width: 768px) {
+        width: 93%;
+      }
+      &-title {
+        margin: 0;
+        background-image: linear-gradient(to right, #fff700, #00c3ff);
+        -webkit-background-clip: text;
+        color: transparent;
+        font-size: 2.5rem;
+        font-weight: 700;
+        @media (min-width: 1440px) {
+          font-size: 3rem;
+        }
+      }
+      &-tech {
+        font-weight: bold;
+      }
+      &-description {
+        text-align: justify;
+      }
+      &-link {
+        color: black;
+        text-decoration: none;
+        font-weight: bold;
+      }
+    }
   }
 }
 </style>
